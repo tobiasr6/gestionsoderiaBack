7155afd2f39d08600635df9731efdd2b4068c61b
tobiasr6 <requenatobias3@gmail.com>
2024-10-01 19:45:31 -0300

    delete: Logger.

--- back-componentes/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-// back-componentes/middleware/logger.js
-const logger = (req, res, next) => {
-    console.log(`${req.method}${req.url} ${res.statusCode} `);
-    next(); // pasa el control al siguiente middleware
-};
-
-module.exports = logger;
