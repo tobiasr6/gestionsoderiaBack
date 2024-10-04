f71caa7ec392cfcb56a74f75f80f0f0d4180cb03
tobiasr6 <requenatobias3@gmail.com>
2024-10-04 00:46:01 -0300

    change: Ruta para eliminar clientes.

--- back-componentes/routes/clientesRoute.js
+++ back-componentes/routes/clientesRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getAllClientes, addCliente } = require('../controllers/clientesController');
+const { getAllClientes, addCliente, deleteCliente } = require('../controllers/clientesController');
 const router = express.Router();
 
 // Rutas GET y POST para clientes
 router.get('/', getAllClientes); // Ruta para obtener todos los clientes
 router.post('/', addCliente); // Ruta para agregar un nuevo cliente
+router.delete('/:idCliente', deleteCliente) //Ruta para eliminar un cliente
 
 module.exports = router;
