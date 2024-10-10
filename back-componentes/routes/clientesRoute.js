28dfd1ffed519c9b0911311ee4f62fb2ab40eef9
tobiasr6 <requenatobias3@gmail.com>
2024-10-10 01:27:07 -0300

    change: Agregado ruta editar.

--- back-componentes/routes/clientesRoute.js
+++ back-componentes/routes/clientesRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getAllClientes, addCliente, deleteCliente } = require('../controllers/clientesController');
+const { getAllClientes, addCliente, deleteCliente, editCliente } = require('../controllers/clientesController');
 const router = express.Router();
 
 // Rutas GET y POST para clientes
 router.get('/', getAllClientes); // Ruta para obtener todos los clientes
 router.post('/', addCliente); // Ruta para agregar un nuevo cliente
 router.delete('/:idCliente', deleteCliente) //Ruta para eliminar un cliente
+router.put('/:idCliente', editCliente) //Ruta para editar un cliente
+
 
 module.exports = router;
