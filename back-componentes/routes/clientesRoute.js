b590b57dc76e30e8cbb4418e217e888132b4915b
tobiasr6 <requenatobias3@gmail.com>
2024-10-14 23:31:22 -0300

    change: Agregado ruta para traer un cliente por id

--- back-componentes/routes/clientesRoute.js
+++ back-componentes/routes/clientesRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllClientes, addCliente, deleteCliente, editCliente } = require('../controllers/clientesController');
+const { getAllClientes, addCliente, deleteCliente, editCliente, getClienteById } = require('../controllers/clientesController');
 const router = express.Router();
 
 // Rutas GET y POST para clientes
@@ -7,6 +7,7 @@ router.get('/', getAllClientes); // Ruta para obtener todos los clientes
 router.post('/', addCliente); // Ruta para agregar un nuevo cliente
 router.delete('/:idCliente', deleteCliente) //Ruta para eliminar un cliente
 router.put('/:idCliente', editCliente) //Ruta para editar un cliente
+router.get('/:idCliente', getClienteById) //Ruta para obtener un cliente por id
 
 
 module.exports = router;
